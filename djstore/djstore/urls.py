a311572976036d954c6adc2ff751281ba5f5d6cc
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2024-09-17 22:29:18 -0400

    added media folder, modified settings, urls

--- djstore/djstore/urls.py
+++ djstore/djstore/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import static
 from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1', include('djoser.urls')),
     path('api/v1', include('djoser.urls.authtoken')),
 
-]
+] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- djstore/product/models.py
+++ djstore/product/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.core.files import File
+from io import BytesIO
+from PIL import Image
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -11,3 +14,43 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return f'/{self.slug}/'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_added',)
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.category.slug}/{self.slug}/'
+
+    def get_image(self):
+        if self.image:
+            return 'http://127.0.0.1:8000' + self.image.url
+        return ''
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return 'http://127.0.0.1:8000' + self.thumbnail.url
+        else:
+            return ''
+        
+    def make_thumbnail(self, image, size=(300, 200)):
+        image = Image.open(self.image)
+        image = image.convert('RGB')
+        image.thumbnail(size)
+        thumb_io = BytesIO()
+        image.save(thumb_io, 'JPEG', quality=85)
+        thumbnail = File(thumb_io, name=self.image.name)
+        self.thumbnail = thumbnail
+        self.save()
