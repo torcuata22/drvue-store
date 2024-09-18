7fd394fc3bc7282b6c1a3f85d7107b500ac93a2a
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2024-09-17 21:56:43 -0400

    added vue, basic vue setup

---
+++ djstore_vue/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">
+          <strong>DjStore</strong>
+        </router-link>
+     
+      
+      <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+
+    </div>
+    <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-end">
+        <router-link to="/about" class="navbar-item">About</router-link>
+        <router-link to="/about" class="navbar-item">News</router-link>
+        <div class="navbar-item">
+          <div class="buttons">
+            <router-link to="/log-in" class="button is-light">Log in</router-link>
+            <router-link to="/log-in" class="button is-success">
+              <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+              <span>My Cart</span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    </nav>
+    <section class="section">
+     <router-view/>
+    </section>
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2024</p>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+  @import '../node_modules/bulma';
+</style>
