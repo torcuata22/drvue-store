99d6c70f269ab0746e1865b34234b2c101a961b2
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2024-09-26 13:59:33 -0400

    categories

--- djstore/product/serializers.py
+++ djstore/product/serializers.py
@@ -13,3 +13,15 @@ class ProductSerializer(serializers.ModelSerializer):
             'get_image',
             'get_thumbnail',
         )
+
+class CategorySerializer(serializers.ModelSerializer):
+    #use product serializer to access products
+    products = ProductSerializer(many=True)
+    class Meta:
+        model = Category
+        fields = (
+            'id',
+            'name',
+            'get_absolute_url',
+            'products', 
+        )
--- djstore/product/views.py
+++ djstore/product/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import ProductSerializer
-from .models import Product
+from .serializers import ProductSerializer, CategorySerializer
+from .models import Product, Category
 
 class LatestProductsList(APIView):
     def get(self, request, format=None):
@@ -11,13 +11,29 @@ class LatestProductsList(APIView):
         return Response(serializer.data)
 
 class ProductDetail(APIView):
+    #check if product exists
     def get_object(self, category_slug, product_slug):  
         try:
             return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
         except Product.DoesNotExist:
             raise Http404
 
+    #override get method
     def get(self, request, category_slug, product_slug, format=None):
         product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+class CategoryDetail(APIView):
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Category.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, category_slug, format=None):
+        print(category_slug)
+        category = self.get_object(category_slug)
+        print(f"Category: {category}")
+        serializer = CategorySerializer(category)
         return Response(serializer.data)
